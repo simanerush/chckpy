f1363c5f4f8b5c08bf94241f84a4e0c15cb961f8
Sima Nerush <2002ssn@gmail.com>
2022-10-24 17:23:16 -0700

    add some new things

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,5 @@
 use parsel::{
-    ast::{Either, Ident, LeftAssoc, LitInt, LitStr, Paren, Punctuated, Token},
+    ast::{Brace, Ident, LeftAssoc, LitInt, LitStr, Paren, Punctuated, Token, Many},
     FromStr, Parse, ToTokens,
 };
 
@@ -7,18 +7,34 @@ mod kw {
     parsel::custom_keyword!(pass);
     parsel::custom_keyword!(print);
     parsel::custom_keyword!(input);
+    parsel::custom_keyword!(def);
 }
 
 /// <prgm> ::= <blck>
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub struct Prgm {
+    pub defns: Many<Defn>, 
     pub main: Blck,
+}
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+pub struct Defn {
+   def: kw::def,
+   name: Ident,
+   params: Paren<Punctuated<Ident, Token!(,)>>,
+   colon: Token!(:),
+   nest: Nest,
+}
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+pub struct Nest {
+    block: Brace<Blck>,
 }
 
 /// <blck> ::= <stmt> EOLN <stmt> OLN
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub struct Blck {
-    pub stmts: Punctuated<Stmt, Token!(;)>,
+    pub stmts: Many<Stmt>,
 }
 
 // <stmt> ::= <name> = <expn>
@@ -30,11 +46,57 @@ pub enum Stmt {
         ident: Ident,
         equals: Token!(=),
         expn: Expn,
+        end: Token!(;),
     },
-    Pass(kw::pass),
-    Print(kw::print, Paren<Expn>),
-}
+    Updt {
+        ident: Ident,
+        equals: Updt,
+        expn: Expn,
+        end: Token!(;),
+    },
+    Pass(kw::pass, Token!(;)),
+    Print(kw::print, Paren<Expn>, Token!(;)),
+    If { 
+        if_: Token!(if),
+        expn: Expn,
+        colon1: Token!(:),
 
+        #[parsel(recursive)]
+        nest1:  Box<Nest>,
+        else_: Token!(else),
+        colon2: Token!(:),
+
+        #[parsel(recursive)]
+        nest2:  Box<Nest>,
+    },
+    While {
+        while_: Token!(while),
+        expn: Expn,
+        colon: Token!(:),
+
+        #[parsel(recursive)]
+        nest: Box<Nest>,
+    },
+    ReturnExpn {
+        return_: Token!(return),
+        expn: Expn,
+        end: Token!(;),
+    },
+    Return {
+        return_: Token!(return),
+        end: Token!(;),
+    },
+    FuncCall {
+        ident: Ident,
+        args: Paren<Punctuated<Ident, Token!(,)>>,
+    }
+   }
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+pub enum Updt {
+    Plus(Token!(+=)),
+    Minus(Token!(-=)),
+}
 // <expn> ::= <addn>
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub struct Expn {
