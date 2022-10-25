17baf73dac260a26824ef2bc56670e064daa981a
Riley Shahar <riley.shahar@gmail.com>
2022-10-24 17:57:59 -0700

    Impl parser

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,8 @@
 use parsel::{
-    ast::{Brace, Ident, LeftAssoc, LitInt, LitStr, Paren, Punctuated, Token, Many},
+    ast::{
+        Brace, Ident, LeftAssoc, LitBool, LitInt, LitStr, Many, Paren, Punctuated, RightAssoc,
+        Token,
+    },
     FromStr, Parse, ToTokens,
 };
 
@@ -7,23 +10,29 @@ mod kw {
     parsel::custom_keyword!(pass);
     parsel::custom_keyword!(print);
     parsel::custom_keyword!(input);
+    parsel::custom_keyword!(int);
     parsel::custom_keyword!(def);
+    parsel::custom_keyword!(str);
+    parsel::custom_keyword!(not);
+    parsel::custom_keyword!(and);
+    parsel::custom_keyword!(or);
+    parsel::custom_keyword!(None);
 }
 
 /// <prgm> ::= <blck>
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub struct Prgm {
-    pub defns: Many<Defn>, 
+    pub defns: Many<Defn>,
     pub main: Blck,
 }
 
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub struct Defn {
-   def: kw::def,
-   name: Ident,
-   params: Paren<Punctuated<Ident, Token!(,)>>,
-   colon: Token!(:),
-   nest: Nest,
+    def: kw::def,
+    name: Ident,
+    params: Paren<Punctuated<Ident, Token!(,)>>,
+    colon: Token!(:),
+    nest: Nest,
 }
 
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
@@ -56,18 +65,18 @@ pub enum Stmt {
     },
     Pass(kw::pass, Token!(;)),
     Print(kw::print, Paren<Expn>, Token!(;)),
-    If { 
+    If {
         if_: Token!(if),
         expn: Expn,
         colon1: Token!(:),
 
         #[parsel(recursive)]
-        nest1:  Box<Nest>,
+        nest1: Box<Nest>,
         else_: Token!(else),
         colon2: Token!(:),
 
         #[parsel(recursive)]
-        nest2:  Box<Nest>,
+        nest2: Box<Nest>,
     },
     While {
         while_: Token!(while),
@@ -89,45 +98,92 @@ pub enum Stmt {
     FuncCall {
         ident: Ident,
         args: Paren<Punctuated<Ident, Token!(,)>>,
-    }
-   }
+    },
+}
 
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
 pub enum Updt {
     Plus(Token!(+=)),
     Minus(Token!(-=)),
 }
+
 // <expn> ::= <addn>
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
-pub struct Expn {
-    pub addn: Addn,
+pub enum Expn {
+    BinOp(
+        LeftBinExp<Add, LeftBinExp<Mult, LeftBinExp<Comp, LeftBinExp<And, LeftBinExp<Or, Leaf>>>>>,
+    ),
+    UnOp(UnExp<Not>),
+    Inpt(kw::input, #[parsel(recursive)] Paren<Box<Expn>>),
+    Int(kw::int, #[parsel(recursive)] Paren<Box<Expn>>),
+    Str(kw::str, #[parsel(recursive)] Paren<Box<Expn>>),
+    FuncCall {
+        name: Ident,
+        args: Paren<Punctuated<Ident, Token!(,)>>,
+    },
 }
 
-// <addn> ::= <mult> <pm> <mult> <pm> ... <pm> <mult>
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
-pub struct Addn {
-    pub mults: LeftAssoc<Pm, Mult>,
+struct LeftBinExp<B: Binop, C> {
+    children: LeftAssoc<B, C>,
 }
 
-// <pm> ::= + | -
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
-pub enum Pm {
-    Addn(Token!(+)),
-    Subt(Token!(-)),
+struct RightBinExp<B: Binop, C> {
+    children: RightAssoc<B, C>,
 }
 
-// <mult> ::= <leaf>  <leaf>  ...  <leaf>
+trait Binop {}
+
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
-pub struct Mult {
-    pub leafs: LeftAssoc<Md, Leaf>,
+enum Add {
+    Plus(Token!(+)),
+    Minus(Token!(-)),
 }
 
-// <md> ::= + | -
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
-pub enum Md {
-    Mult(Token!(*)),
-    Divn(Token!(/)),
+enum Mult {
+    Times(Token!(*)),
+    Div(Token!(/)),
+    Mod(Token!(%)),
 }
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+enum Comp {
+    Lt(Token!(<)),
+    Leq(Token!(<=)),
+    Eq(Token!(==)),
+}
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+struct And(kw::and);
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+struct Or(kw::or);
+
+impl Binop for Add {}
+
+impl Binop for Mult {}
+
+impl Binop for Comp {}
+
+impl Binop for And {}
+
+impl Binop for Or {}
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+struct UnExp<U: Unop> {
+    op: U,
+    #[parsel(recursive)]
+    expn: Box<Expn>,
+}
+
+trait Unop {}
+
+#[derive(PartialEq, Eq, Debug, Parse, ToTokens, FromStr)]
+struct Not(kw::not);
+
+impl Unop for Not {}
 
 // <leaf> ::= <name> | <nmbr> | input ( <strg> ) | ( <expn> )
 // <name> ::= x | count | _special | y0 | camelWalk | snake_slither | ...
@@ -135,8 +191,10 @@ pub enum Md {
 // <strg> ::= "hello" | "" | "say \"yo!\n\tyo.\"" | ...
 #[derive(PartialEq, Eq, Debug, Parse, ToTokens)]
 pub enum Leaf {
-    Inpt(kw::input, Paren<LitStr>),
     Expn(#[parsel(recursive)] Paren<Box<Expn>>),
     Nmbr(LitInt),
+    Strg(LitStr),
     Name(Ident),
+    Bool(LitBool),
+    Unit(kw::None),
 }
