faf113026aa01052e5afc9f5626e01a71fd571f7
Sima Nerush <2002ssn@gmail.com>
2022-11-20 15:42:38 -0800

    Finish check trait

--- src/check.rs
+++ src/check.rs
@@ -1,3 +1,29 @@
-trait Check {
+use crate::eval::Error;
 
+trait Check {
+    fn chck(expected: Rtns, definitions: &mut DefTypes, sym_table: &mut SymbolTable) -> Result<Rtns, Error>;
+}
+
+enum Rtns {
+    Fallthrough,
+    MightReturn(Ty),
+    Returns(Ty),
+}
+
+enum Ty {
+    Int,
+    Bool,
+    Str,
+    Unit,
+}
+
+pub struct ArrowType {
+    pub return_type: Option<Ty>,
+    pub params: Vec<Ty>
+}
+
+pub struct DefTypes(HashMap<Ident, ArrowType>);
+
+pub struct SymbolTable {
+    pub table: HashMap<Ident, Ty>
 }
